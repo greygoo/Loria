d679cd332fe4537fa93a5da67a30d690f7dc77d7
greygoo <chuller@suse.de>
2015-05-05 09:17:37 +0200

    Add article controller tests to verify authentication.

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -27,4 +27,19 @@ describe ArticlesController do
       expect(response).to render_template :show
     end
   end
+
+  describe "POST #create" do
+    context "without authentication" do
+      it "does not create an article" do
+        expect{
+          post :create, article: FactoryGirl.attributes_for(:article)
+        }.to change(Article, :count).by(0)
+      end 
+
+      it "responds with 401 unauthorized" do
+        post :create, article: FactoryGirl.attributes_for(:article)
+        expect(response.response_code).to eq 401
+      end
+    end
+  end
 end
